e2ea964c044cc092d4753233bf7ba2fbd179cbad
Axel Rikte <axel.rikte98@gmail.com>
2018-11-12 19:25:00 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("This is a sudoku solver, input your sudoku");
+
+var rad1 = [0,0,0,0,0,0,0,0,0];
+var rad2 = [0,0,0,0,0,0,0,0,0];
+var rad3 = [0,0,0,0,0,0,0,0,0];
+var rad4 = [0,0,0,0,0,0,0,0,0];
+var rad5 = [0,0,0,0,0,0,0,0,0];
+var rad6 = [0,0,0,0,0,0,0,0,0];
+var rad7 = [0,0,0,0,0,0,0,0,0];
+var rad8 = [0,0,0,0,0,0,0,0,0];
+var rad9 = [0,0,0,0,0,0,0,0,0];
+var sudoku = [rad1,rad2,rad3,rad4,rad5,rad6,rad7,rad8,rad9];
+
+console.log(sudoku);
